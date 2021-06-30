58002595425c3dd94ff69b61258cd380386fcc8a
Nasim <f.i.nasim96@gmail.com>
2021-06-30 07:05:59 +0600

    First Commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="styles.css" type="text/css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 header">
+                    <h1>Amazone to Bangladesh</h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4 col-sm-12 product">
+                    <div>
+                        <a href="https://www.amazon.com/LG-27GL83A-B-Ultragear-Compatible-Monitor/dp/B07YGZL8XF/ref=lp_16225007011_1_1">
+                            <img src="images/01.jpg">
+                        </a>
+                    </div>
+                    <h2>40000 BDT</h2>
+                    <a href="https://www.amazon.com/LG-27GL83A-B-Ultragear-Compatible-Monitor/dp/B07YGZL8XF/ref=lp_16225007011_1_1">
+                        <p>LG 27GL83A-B 27 Inch Ultragear QHD IPS 1ms NVIDIA G-SYNC Compatible Gaming Monitor, Black </p>
+                    </a>
+                </div>
+                <div class="col-md-4 col-sm-12 product">
+                    <div>
+                        <a href="https://www.amazon.com/Roku-Express-Streaming-Media-Player/dp/B07WVFCVJN/ref=lp_16225007011_1_2">
+                            <img src="images/02.jpg">
+                        </a>
+                    </div>
+                    <h2>2400 BDT</h2>
+                    <a href="https://www.amazon.com/Roku-Express-Streaming-Media-Player/dp/B07WVFCVJN/ref=lp_16225007011_1_2">
+                        <p>Roku Express | HD Streaming Media Player with High Speed HDMI Cable and Simple Remote </p>
+                    </a>
+                </div>
+                <div class="col-md-4 col-sm-12 product">
+                    <div>
+                        <a href="https://www.amazon.com/Roku-Streaming-Stick-HDR-Streaming-Long-range/dp/B075XLWML4/ref=lp_16225007011_1_3">
+                            <img src="images/03.jpg">
+                        </a>
+                    </div>
+                    <h2>3200 BDT</h2>
+                    <a href="https://www.amazon.com/Roku-Streaming-Stick-HDR-Streaming-Long-range/dp/B075XLWML4/ref=lp_16225007011_1_3">
+                        <p>Roku Streaming Stick+ | HD/4K/HDR Streaming Device with Long-range Wireless and Voice Remote with TV Controls </p>
+                    </a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4 col-sm-12 product">
+                    <div>
+                        <a href="https://www.amazon.com/Samsung-Electronics-microSDXC-Adapter-MB-ME256HA/dp/B0887P21Z2/ref=lp_16225007011_1_4">
+                            <img src="images/04.jpg">
+                        </a>
+                    </div>
+                    <h2>3200 BDT</h2>
+                    <a href="https://www.amazon.com/Samsung-Electronics-microSDXC-Adapter-MB-ME256HA/dp/B0887P21Z2/ref=lp_16225007011_1_4">
+                        <p>SAMSUNG ELECTRONICS EVO Select 256GB MicroSDXC UHS-I U3 100MB/s Full HD & 4K UHD Memory Card with Adapter (MB-ME256HA) </p>
+                    </a>
+                </div>
+                <div class="col-md-4 col-sm-12 product">
+                    <div>
+                        <a href="https://www.amazon.com/Seagate-Portable-External-Hard-Drive/dp/B07CRG94G3/ref=lp_16225007011_1_5">
+                            <img src="images/05.jpg">
+                        </a>
+                    </div>
+                    <h2>4800 BDT</h2>
+                    <a href="https://www.amazon.com/Seagate-Portable-External-Hard-Drive/dp/B07CRG94G3/ref=lp_16225007011_1_5">
+                        <p>Seagate Portable 2TB External Hard Drive Portable HDD – USB 3.0 for PC, Mac, PS4, & Xbox - 1-Year Rescue Service (STGX2000400) </p>
+                    </a>
+                </div>
+                <div class="col-md-4 col-sm-12 product">
+                    <div>
+                        <a href="https://www.amazon.com/Lenovo-Processor-Graphics-Included-81X20005US/dp/B086226DDB/ref=lp_16225007011_1_6">
+                            <img src="images/06.jpg">
+                        </a>
+                    </div>
+                    <h2>55000 BDT</h2>
+                    <a href="https://www.amazon.com/Lenovo-Processor-Graphics-Included-81X20005US/dp/B086226DDB/ref=lp_16225007011_1_6">
+                        <p>Lenovo Flex 5 14" 2-in-1 Laptop, 14.0" FHD (1920 x 1080) Touch Display, AMD Ryzen 5 4500U Processor, 16GB DDR4, 256GB SSD, AMD Radeon Graphics, Digital Pen Included, Win 10, 81X20005US, Graphite Grey </p>
+                    </a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4 col-sm-12 product">
+                    <div>
+                        <a href="https://www.amazon.com/Acer-Octa-Core-Graphics-Fingerprint-SF314-42-R9YN/dp/B086KKKT15/ref=lp_16225007011_1_7">
+                            <img src="images/07.jpg">
+                        </a>
+                    </div>
+                    <h2>55000 BDT</h2>
+                    <a href="https://www.amazon.com/Acer-Octa-Core-Graphics-Fingerprint-SF314-42-R9YN/dp/B086KKKT15/ref=lp_16225007011_1_7">
+                        <p>Acer Swift 3 Thin & Light Laptop, 14" Full HD IPS, AMD Ryzen 7 4700U Octa-Core with Radeon Graphics, 8GB LPDDR4, 512GB NVMe SSD, Wi-Fi 6, Backlit KB, Fingerprint Reader, Alexa Built-in </p>
+                    </a>
+                </div>
+                <div class="col-md-4 col-sm-12 product">
+                    <div>
+                        <a href="https://www.amazon.com/Acer-A515-55-35SE-i3-1005G1-Processor-Fingerprint/dp/B08HN1WDWZ/ref=lp_16225007011_1_8">
+                            <img src="images/08.jpg">
+                        </a>
+                    </div>
+                    <h2>36000 BDT</h2>
+                    <a href="https://www.amazon.com/Acer-A515-55-35SE-i3-1005G1-Processor-Fingerprint/dp/B08HN1WDWZ/ref=lp_16225007011_1_8">
+                        <p>Acer Aspire 5 A515-55-35SE, 15.6" Full HD Display, 10th Gen Intel Core i3-1005G1 Processor, 4GB DDR4, 128GB NVMe SSD, Intel WiFi 6 AX201, Backlit KB, Fingerprint Reader, Windows 10 Home (S Mode) </p>
+                    </a>
+                </div>
+                <div class="col-md-4 col-sm-12 product">
+                    <div>
+                        <a href="https://www.amazon.com/Lenovo-Chromebook-MediaTek-Processor-81JW0000US/dp/B07GLV1VC7/ref=lp_16225007011_1_9">
+                            <img src="images/09.jpg">
+                        </a>
+                    </div>
+                    <h2>35000 BDT</h2>
+                    <a href="https://www.amazon.com/Lenovo-Chromebook-MediaTek-Processor-81JW0000US/dp/B07GLV1VC7/ref=lp_16225007011_1_9">
+                        <p>Lenovo Chromebook S330 Laptop, 14-Inch FHD (1920 x 1080) Display, MediaTek MT8173C Processor, 4GB LPDDR3, 64GB eMMC, Chrome OS, 81JW0000US, Business Black </p>
+                    </a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4 col-sm-12 product">
+                    <div>
+                        <a href="https://www.amazon.com/SanDisk-128GB-Extreme-microSD-Adapter/dp/B07FCMKK5X/ref=lp_16225007011_1_9">
+                            <img src="images/10.jpg">
+                        </a>
+                    </div>
+                    <h2>1800 BDT</h2>
+                    <a href="https://www.amazon.com/SanDisk-128GB-Extreme-microSD-Adapter/dp/B07FCMKK5X/ref=lp_16225007011_1_9">
+                        <p>SanDisk 128GB Extreme MicroSDXC UHS-I Memory Card with Adapter - C10, U3, V30, 4K, A2, Micro SD - SDSQXA1-128G-GN6MA </p>
+                    </a>
+                </div>
+                <div class="col-md-4 col-sm-12 product">
+                    <div>
+                        <a href="https://www.amazon.com/Acer-A515-46-R14K-Quad-Core-Processor-Backlit/dp/B08VKNVDDR/ref=lp_16225007011_1_10">
+                            <img src="images/11.jpg">
+                        </a>
+                    </div>
+                    <h2>35000 BDT</h2>
+                    <a href="https://www.amazon.com/Acer-A515-46-R14K-Quad-Core-Processor-Backlit/dp/B08VKNVDDR/ref=lp_16225007011_1_10">
+                        <p>Acer Aspire 5 A515-46-R14K Slim Laptop | 15.6" Full HD IPS | AMD Ryzen 3 3350U Quad-Core Mobile Processor | 4GB DDR4 | 128GB NVMe SSD | WiFi 6 | Backlit KB | Amazon Alexa | Windows 10 Home (S Mode) </p>
+                    </a>
+                </div>
+                <div class="col-md-4 col-sm-12 product">
+                    <div>
+                        <a href="https://www.amazon.com/HP-Chromebook-Processor-Lightweight-14a-na0021nr/dp/B08YKGZF7P/ref=lp_16225007011_1_12">
+                            <img src="images/12.jpg">
+                        </a>
+                    </div>
+                    <h2>25000 BDT</h2>
+                    <a href="https://www.amazon.com/HP-Chromebook-Processor-Lightweight-14a-na0021nr/dp/B08YKGZF7P/ref=lp_16225007011_1_12">
+                        <p>HP Chromebook 14 Laptop, Intel Celeron N4000 Processor, 4 GB RAM, 32 GB eMMC, 14” HD Display, Chrome, Lightweight Computer with Webcam and Dual Mics, Home, School, Music, Movies (14a-na0021nr, 2021) </p>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
